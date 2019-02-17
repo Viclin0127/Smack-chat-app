c4d701cd358b0a6e5795fb2e78ed81ad30b5ba28
YuanHungLin <viclin@linyuanhongde-MacBook-Pro.local>
2019-02-17 16:38:29 +0800

    create LoginVC and set UI interface first

---
+++ Smack/Smack/Controller/LoginVC.swift
@@ -0,0 +1,19 @@
+//
+//  LoginVC.swift
+//  Smack
+//
+//  Created by YuanHungLin on 2019/2/17.
+//  Copyright © 2019 linyuanhung. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+}
---
+++ Smack/Smack/Utilities/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by YuanHungLin on 2019/2/16.
+//  Copyright © 2019 linyuanhung. All rights reserved.
+//
+
+import Foundation
+
+let TO_LOGIN = "toLogin"
